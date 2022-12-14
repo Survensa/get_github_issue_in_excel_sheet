6eba6df946c0cc846bf45ca462abd37a90ad975c
Suraj Seenivasan <95559513+Survensa@users.noreply.github.com>
2022-12-14 18:44:12 +0530

    Update one_repo_gitissue.py

--- one_repo_gitissue.py
+++ one_repo_gitissue.py
@@ -2,19 +2,18 @@ from github import Github
 import pandas as pd
 
 # Get the repository
-g = Github("<GIT_ACCESS_TOKEN")
+g = Github("<GIT_ACCESS_TOKEN>")
 
-repo = g.get_repo("project-chip/connectedhomeip")
+repo = g.get_repo("<OWNER>/<REPO>")
 filename = repo.name + datetime.now().strftime("%m_%d_%Y_%H_%M_%S") + ".xlsx"
 # store the data in the list
 data = []
 
 for issue in repo.get_issues(state="all"):
-    data.append([repo_name, issue.number, issue.labels, issue.created_at, issue.state, issue.title, issue.closed_at, issue.html_url])
+    data.append([repo.name, issue.number, issue.labels, issue.created_at, issue.state, issue.title, issue.closed_at, issue.html_url])
 
 df = pd.DataFrame(data, columns=["Repo Name", "Issue ID", "Label", "Created Date", "State", "Title", "Closed Date", "URL"])
 df["Label"] = df["Label"].apply(lambda x: [label.name for label in x])
 df["Label"] = df["Label"].apply(lambda x: '"{0}"'.format(", ".join(x)))
 df.to_excel(filename, index=False)
-writer.close()
 print("Issue is saved in :", filename)
