55c1fef77ba9a47104ea04d4e2be16b361370301
Suraj Seenivasan <suraj@graniteriverlabs.in>
2023-08-16 11:28:57 +0530

    Update pull_issue_from_github.py

--- src/pull_issue_from_github.py
+++ src/pull_issue_from_github.py
@@ -1,11 +1,11 @@
 import time
 import os
-import github
 import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
-import yaml
 import json
+import requests
+import yaml
 
 def format_duration(seconds):
     hours, remainder = divmod(seconds, 3600)
@@ -19,71 +19,63 @@ repo_names = yaml_data["repos"]
 github_token = os.environ.get("MY_GITHUB_TOKEN")
 service_account_json = os.environ.get("SERVICE_ACCOUNT_JSON")
 
-g = github.Github(github_token)
-service_account_json_dict = json.loads(service_account_json)
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_dict(service_account_json_dict, scope)
+api_base_url = "https://api.github.com"
+headers = {
+    "Authorization": f"Bearer {github_token}",
+    "Accept": "application/vnd.github.v3+json"
+}
+
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(json.loads(service_account_json), [
+    'https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive'])
 gc = gspread.authorize(credentials)
 
 api_request_delay = 60  # Adjust as needed, in seconds
 
 for repo_name in repo_names:
-    repo = g.get_repo(repo_name)
-    repo_name_clean = repo.name  # Avoid reassigning to repo_name
     start_time = time.time()
-    print("Fetching issues for repo:", repo_name_clean)
+    print("Fetching issues for repo:", repo_name)
+    issues_url = f"{api_base_url}/repos/{repo_name}/issues"
+    issues_response = requests.get(issues_url, headers=headers)
+    issues_data = issues_response.json()
     
-    issue_list = []
+    df = pd.DataFrame([
+        [repo_name, issue['number'], issue['state'], issue['title'], issue['user']['login'],
+         ", ".join([label['name'] for label in issue['labels']]),
+         issue['created_at'], issue.get('closed_at', None), issue['html_url']] for issue in issues_data],
+        columns=["Repo Name", "Issue ID", "State", "Title", "Author", "Label", "Created Date", "Closed Date", "URL"])
     
-    for issue in repo.get_issues(state="all"):
-        labels = [label.name for label in issue.labels]
-        issue_dict = {
-            "Repo Name": repo_name_clean,
-            "Issue ID": issue.number,
-            "State": issue.state,
-            "Title": issue.title,
-            "Author": issue.user.login,
-            "Labels": labels,
-            "Created Date": issue.created_at,
-            "Closed Date": issue.closed_at,
-            "URL": issue.html_url
-        }
-        issue_list.append(issue_dict)
-
-    df = pd.DataFrame(issue_list, columns=["Repo Name", "Issue ID", "State", "Title", "Author", "Labels", "Created Date", "Closed Date", "URL"])
-    df["Labels"] = df["Labels"].apply(lambda x: ", ".join(x) if x else None)
+    df["Label"] = df["Label"].apply(lambda x: '"{0}"'.format(", ".join([label.name for label in x])) if x else None)
     df["Created Date"] = df["Created Date"].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'))
-    df["Closed Date"] = df["Closed Date"].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S') if x and not pd.isnull(x) else None)
-    
+    df["Closed Date"] = df["Closed Date"].apply(
+        lambda x: x.strftime('%Y-%m-%d %H:%M:%S') if x and not pd.isnull(x) else None)
     end_time = time.time()
     duration_seconds = end_time - start_time
     duration = format_duration(duration_seconds)
-    print(f"Fetch Completed for repo {repo_name_clean}: {duration}")
-    print("Processing issues for repo:", repo_name_clean)
+    print(f"Fetch Completed for repo {repo_name}: {duration}")
+    print("Processing issues for repo:", repo_name)
 
     time.sleep(api_request_delay)
 
     sh = gc.open("Matterissues")
-    worksheet_name = f"{repo_name_clean}_issues"
-    
+    worksheet_name = f"{repo_name}_issues"
     try:
         worksheet = sh.worksheet(worksheet_name)
         worksheet.clear()
     except gspread.exceptions.WorksheetNotFound:
         worksheet = sh.add_worksheet(title=worksheet_name, rows=str(len(df) + 1), cols=9)
-        print(f"Created a worksheet named {worksheet_name} for the repo {repo_name_clean}")
+        print(f"Created a worksheet named {worksheet_name} for the repo {repo_name}")
     
-    header_cells = worksheet.range(1, 1, 1, 9)
-    for column_name, cell in zip(df.columns, header_cells):
-        cell.value = column_name
-    worksheet.update_cells(header_cells)
+    cell_list = worksheet.range(1, 1, 1, 9)
+    for t, cell in zip(df.columns, cell_list):
+        cell.value = t
+    worksheet.update_cells(cell_list)
     
-    data_cells = worksheet.range(2, 1, len(df) + 1, 9)
-    for cell_value, cell in zip(df.values.flatten(), data_cells):
-        cell.value = cell_value
-    worksheet.update_cells(data_cells)
+    cell_list = worksheet.range(2, 1, len(df) + 1, 9)
+    for t, cell in zip(df.values.flatten(), cell_list):
+        cell.value = t
+    worksheet.update_cells(cell_list)
     
-    print(f"Updated the sheet {worksheet_name} with {repo_name_clean} repo issues")
+    print(f"Updated the sheet {worksheet_name} with {repo_name} repo issues")
     
     time.sleep(api_request_delay)
 
