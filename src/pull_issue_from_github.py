279b9e223bbba42df922dc887e51880355cfdac9
Suraj Seenivasan <suraj@graniteriverlabs.in>
2023-08-16 10:49:50 +0530

    Update pull_issue_from_github.py

--- src/pull_issue_from_github.py
+++ src/pull_issue_from_github.py
@@ -34,51 +34,49 @@ api_request_delay = 60  # Adjust as needed, in seconds
 for repo_name in repo_names:
     start_time = time.time()
     print("Fetching issues for repo:", repo_name)
-    
     issues_url = f"{api_base_url}/repos/{repo_name}/issues"
     issues_response = requests.get(issues_url, headers=headers)
-    if issues_response.status_code == 200:
-        issues_data = issues_response.json()
-        
-        df = pd.DataFrame([
-            [repo_name, issue['number'], issue['state'], issue['title'], issue['user']['login'],
-             ", ".join([label['name'] for label in issue['labels']]),
-             issue['created_at'], issue.get('closed_at', None), issue['html_url']] for issue in issues_data],
-            columns=["Repo Name", "Issue ID", "State", "Title", "Author", "Label", "Created Date", "Closed Date", "URL"])
-        
-        df["Label"] = df["Label"].apply(lambda x: '"{0}"'.format(", ".join([label.name for label in x])) if x else None)
-        df["Created Date"] = df["Created Date"].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'))
-        df["Closed Date"] = df["Closed Date"].apply(
-            lambda x: x.strftime('%Y-%m-%d %H:%M:%S') if x and not pd.isnull(x) else None)
-        end_time = time.time()
-        duration_seconds = end_time - start_time
-        duration = format_duration(duration_seconds)
-        print(f"Fetch Completed for repo {repo_name}: {duration}")
-        print("Processing issues for repo:", repo_name)
+    issues_data = issues_response.json()
+    
+    df = pd.DataFrame([
+        [repo_name, issue['number'], issue['state'], issue['title'], issue['user']['login'],
+         ", ".join([label['name'] for label in issue['labels']]),
+         issue['created_at'], issue.get('closed_at', None), issue['html_url']] for issue in issues_data],
+        columns=["Repo Name", "Issue ID", "State", "Title", "Author", "Label", "Created Date", "Closed Date", "URL"])
+    
+    df["Label"] = df["Label"].apply(lambda x: '"{0}"'.format(", ".join([label.name for label in x])) if x else None)
+    df["Created Date"] = df["Created Date"].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'))
+    df["Closed Date"] = df["Closed Date"].apply(
+        lambda x: x.strftime('%Y-%m-%d %H:%M:%S') if x and not pd.isnull(x) else None)
+    end_time = time.time()
+    duration_seconds = end_time - start_time
+    duration = format_duration(duration_seconds)
+    print(f"Fetch Completed for repo {repo_name}: {duration}")
+    print("Processing issues for repo:", repo_name)
 
-        time.sleep(api_request_delay)
+    time.sleep(api_request_delay)
 
-        sh = gc.open("Matterissues")
-        worksheet_name = f"{repo_name}_issues"
-        try:
-            worksheet = sh.worksheet(worksheet_name)
-            worksheet.clear()
-        except gspread.exceptions.WorksheetNotFound:
-            worksheet = sh.add_worksheet(title=worksheet_name, rows=str(len(df) + 1), cols=9)
-            print(f"Created a worksheet named {worksheet_name} for the repo {repo_name}")
-        
-        cell_list = worksheet.range(1, 1, 1, 9)
-        for t, cell in zip(df.columns, cell_list):
-            cell.value = t
-        worksheet.update_cells(cell_list)
-        
-        cell_list = worksheet.range(2, 1, len(df) + 1, 9)
-        for t, cell in zip(df.values.flatten(), cell_list):
-            cell.value = t
-        worksheet.update_cells(cell_list)
-        
-        print(f"Updated the sheet {worksheet_name} with {repo_name} repo issues")
-        
-        time.sleep(api_request_delay)
+    sh = gc.open("Matterissues")
+    worksheet_name = f"{repo_name}_issues"
+    try:
+        worksheet = sh.worksheet(worksheet_name)
+        worksheet.clear()
+    except gspread.exceptions.WorksheetNotFound:
+        worksheet = sh.add_worksheet(title=worksheet_name, rows=str(len(df) + 1), cols=9)
+        print(f"Created a worksheet named {worksheet_name} for the repo {repo_name}")
+    
+    cell_list = worksheet.range(1, 1, 1, 9)
+    for t, cell in zip(df.columns, cell_list):
+        cell.value = t
+    worksheet.update_cells(cell_list)
+    
+    cell_list = worksheet.range(2, 1, len(df) + 1, 9)
+    for t, cell in zip(df.values.flatten(), cell_list):
+        cell.value = t
+    worksheet.update_cells(cell_list)
+    
+    print(f"Updated the sheet {worksheet_name} with {repo_name} repo issues")
+    
+    time.sleep(api_request_delay)
 
 print("All repos processed")
